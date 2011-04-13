0f997ed325cf6ccd515d1d792b25c474e039a626
BJ Homer <bjhomer@gmail.com>
2011-04-13 00:47:49 -0600

    Initial Commit

---
+++ HSCountingInputStream/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HSCountingInputStream
+//
+//  Created by BJ Homer on 4/13/11.
+//  Copyright 2011 BJ Homer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+	// insert code here...
+	NSLog(@"Hello, World!");
+
+	[pool drain];
+    return 0;
+}
+
