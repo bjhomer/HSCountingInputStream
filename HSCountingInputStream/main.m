25aa1dc7eaac54f442df8718cb5f4456eece6fa0
BJ Homer <bjhomer@gmail.com>
2011-04-14 22:41:52 -0600

    Added working example.

--- HSCountingInputStream/main.m
+++ HSCountingInputStream/main.m
@@ -7,16 +7,54 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "HSCountingInputStream.h"
+
+void downloadFile();
 
 int main (int argc, const char * argv[])
 {
 
 	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-
-	// insert code here...
-	NSLog(@"Hello, World!");
+	
+	downloadFile();
 
 	[pool drain];
     return 0;
+}
+
+void downloadFile() {
+	
+	// A note: I'm using CFReadStreamCreateForHTTPRequest() here because it's the simplest way I know of
+	// to use an input stream without requiring a web service running. This works just as well with an input
+	// stream used as the message of an HTTP request.
+	
+	CFURLRef url = CFURLCreateWithString(NULL, CFSTR("http://bjhomer.blogspot.com"), NULL);
+	CFHTTPMessageRef message = CFHTTPMessageCreateRequest(NULL, CFSTR("GET"), url, kCFHTTPVersion1_1);
+	
+	CFReadStreamRef readStream = CFReadStreamCreateForHTTPRequest(NULL, message);
+	
+	HSCountingInputStream *countingStream = [[HSCountingInputStream alloc] initWithInputStream:(NSInputStream *)readStream];
+	countingStream.characterToCount = 'n';
+	
+	CFReadStreamScheduleWithRunLoop((CFReadStreamRef)countingStream, CFRunLoopGetCurrent(), kCFRunLoopCommonModes);
+	
+	CFReadStreamOpen(readStream);
+	
+	uint8_t buffer[1024];
+	while (CFReadStreamGetStatus(readStream) != kCFStreamStatusAtEnd) {
+		[countingStream read:buffer maxLength:1024];
+	}
+	
+	CFReadStreamClose(readStream);
+	
+	CFReadStreamUnscheduleFromRunLoop((CFReadStreamRef)countingStream, CFRunLoopGetCurrent(), kCFRunLoopCommonModes);
+	
+	NSLog(@"There were %u '%c's in bjhomer.blogspot.com's home page", (int)countingStream.countedCharacters, countingStream.characterToCount);
+	
+	CFRelease(url);
+	CFRelease(message);
+	CFRelease(readStream);
+	[countingStream release];
+	
 }
 
